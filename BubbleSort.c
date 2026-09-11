51d17f25e1faabb93f5460ea8606c9716ece0a61
brozone700 <brozone700@gmail.com>
2026-09-11 19:41:16 +0000

    Going to work, will finish later

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 const int MAX=9;
 
-void printValues(int*);
-void sort(int*);
-void swap(int*, int*);
+void printValues(int* Values) {
+	printf("[");
+	for ( int i = 0; i < MAX; i++) {
+		printf(" %d,", Values[i]);
+	}
+	printf("]\n");
+}
+void sort(int* NumSet){
+	int KeepGoing = 1;
+	while ( KeepGoing > 0) {
+		KeepGoing = 0;
+		for ( int i = 0; i < MAX; i++) {
+			if (NumSet[i] < NumSet[i+1]){
+				KeepGoing++;
+				swap(&NumSet[i], &NumSet[i+1]);
+			}
+		}	
+	}
+}
+void swap(int* num1, int* num2) {
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
 
 int main(){
   int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
