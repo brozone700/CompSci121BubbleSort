44cddb64811c8013caa2a0e539f2dcd1fcf55b76
brozone700 <brozone700@gmail.com>
2026-09-12 01:37:41 +0000

    finished everything after getting off of work

--- BubbleSort.c
+++ BubbleSort.c
@@ -3,41 +3,35 @@ const int MAX=9;
 
 void printValues(int* Values) {
 	printf("[");
-	for ( int i = 0; i < MAX; i++) {
+	for ( int i = 0; i < MAX-1; i++) {
 		printf(" %d,", Values[i]);
 	}
-	printf("]\n");
+	printf(" %d]\n", Values[MAX-1]);
 }
+void swap(int* num1, int* num2) {
+        int temp = *num1;
+        *num1 = *num2;
+        *num2 = temp;
+}
+
 void sort(int* NumSet){
 	int KeepGoing = 1;
 	while ( KeepGoing > 0) {
 		KeepGoing = 0;
-		for ( int i = 0; i < MAX; i++) {
-			if (NumSet[i] < NumSet[i+1]){
+		for ( int i = 0; i < MAX-1; i++) {
+			if (NumSet[i] > NumSet[i+1]){
 				KeepGoing++;
-				swap(&NumSet[i], &NumSet[i+1]);
+				swap(&NumSet[i+1], &NumSet[i]);
 			}
 		}	
 	}
 }
-void swap(int* num1, int* num2) {
-	int temp = *num1;
-	*num1 = *num2;
-	*num2 = temp;
-}
 
 int main(){
   int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
-  printf("Before: \n");
+  printf("\nBefore: \n");
   printValues(values);
-
-  // test swap
-  int x = 3;
-  int y = 5;
-  printf("x: %d, y: %d \n", x, y);
-  swap(&x, &y);
-  printf("x: %d, y: %d \n", x, y);
-
+  printf("\n");
   sort(values);
   printf("After: \n");
   printValues(values);
--- README.md
+++ README.md
@@ -8,4 +8,40 @@ function sort (array):
         for j from zero to MAX - 1:
             if array[j] > array[j+1]:
                 swap array[j] with array[j+1]
-                printArray(array)
+                printArray(array)onstant MAX is max length of array
+
+function printValues(array):
+    print "["
+    for i from 0 to MAX - 2:
+        print array[i], followed by ","
+    print array[MAX - 1]
+    print "]"
+
+function swap(num1, num2):
+    create integer variable temp, set to num1
+    set num1 to num2
+    set num2 to temp
+
+function sort(array):
+    create integer variable KeepGoing, set to 1
+    create integer variable i
+
+    while KeepGoing > 0:
+        set KeepGoing to 0
+        for i from 0 to MAX - 2:
+            if array[i] > array[i+1]:
+                increment KeepGoing
+                swap(array[i+1], array[i])
+
+function main():
+    create array values, set to [7, 3, 9, 4, 6, 1, 2, 8, 5]
+
+    print "Before:"
+    printValues(values)
+
+    sort(values)
+
+    print "After:"
+    printValues(values)
+
+    return 0
